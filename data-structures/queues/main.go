c9609a8c5f99e67b8a8b6a391abbd3de21d6a205
oumagoumag <ouma.godwin10@gmail.com>
2025-07-25 14:10:48 +0300

    feate: Add Enqueue method that adds elements to the end of the queue

--- data-structures/queues/main.go
+++ data-structures/queues/main.go
@@ -8,3 +8,9 @@ import (
 type Queue[T any] struct {
 	slice []T
 }
+
+// Enqueue add an element to the end of the queue.
+func (q *Queue[T]) Enqueue(value T) {
+	q.slice = append(q.slice, value)
+}
+
