e375448f378b539c8f1fa15aa3311b56bd4c742d
oumagoumag <ouma.godwin10@gmail.com>
2025-07-25 14:24:33 +0300

    Feat: Add Dequeue to remove and return the element at the front of the queue

--- data-structures/queues/main.go
+++ data-structures/queues/main.go
@@ -1,16 +1,26 @@
 package main
 
-import (
-	
-)
+import "errors"
 
 // MyQueue is a generic queue implementation using a slice
-type Queue[T any] struct {
+type MyQueue[T any] struct {
 	slice []T
 }
 
 // Enqueue add an element to the end of the queue.
-func (q *Queue[T]) Enqueue(value T) {
+func (q *MyQueue[T]) Enqueue(value T) {
 	q.slice = append(q.slice, value)
 }
 
+// Dequeue removes and returns the element feom the front of the queue
+func (q *MyQueue[T]) Dequeue() (T, error) {
+	if len(q.slice) == 0 {
+		var zero T
+		return zero, errors.New("queue is empty")
+	}
+
+	value := q.slice[0]
+	q.slice = q.slice[1:]
+	return value, nil
+
+}
